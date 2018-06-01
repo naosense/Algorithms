5ebecceff17a1e88f32b592c2519ac2c6d4494e0
pingao <pingao777@gmail.com>
2018-06-01 16:39:55 +0800

    第三周作业初始化

---
+++ src/main/java/com/pingao/assignment/week3/BruteCollinearPoints.java
@@ -0,0 +1,21 @@
+package com.pingao.assignment.week3;
+
+/**
+ * Created by pingao on 2018/5/31.
+ */
+public class BruteCollinearPoints {
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+
+    }
+}
