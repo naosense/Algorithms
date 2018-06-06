f7c429eec828cf09005a89a36c06923ae3d44ae2
pingao <pingao777@gmail.com>
2018-06-06 16:13:57 +0800

    使用系统变量来访问文件

--- src/main/java/com/pingao/assignment/week3/FastCollinearPoints.java
+++ src/main/java/com/pingao/assignment/week3/FastCollinearPoints.java
@@ -4,7 +4,6 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.io.File;
 import java.util.Arrays;
 
 
@@ -108,7 +107,7 @@ public class FastCollinearPoints {
     public static void main(String[] args) {
 
         // read the n points from a file
-        In in = new In(new File("D:\\project\\Algorithms\\src\\test\\resources\\week3-input8.txt"));
+        In in = new In(System.getProperty("user.dir") + "/src/test/resources/week3-input8.txt");
         int n = in.readInt();
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
