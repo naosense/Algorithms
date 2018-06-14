e15f55b1fe2e4cc111b089094f5b5a8c0d9f7d32
pingao <pingao777@gmail.com>
2018-06-14 16:27:14 +0800

    重新使用了原来的排重方法，修复了某些点集下不能正确扫描的bug，得分100

--- src/main/java/com/pingao/assignment/week3/FastCollinearPoints.java
+++ src/main/java/com/pingao/assignment/week3/FastCollinearPoints.java
@@ -21,10 +21,9 @@ public class FastCollinearPoints {
         segments = new ResizingArrayQueue<>();
         Point[] pointsCopy = points.clone();
 
-        for (int i = 0; i < len; i++) {
-            Point p = pointsCopy[i];
-            Arrays.sort(pointsCopy, i + 1, len, p.slopeOrder().thenComparing(Point::compareTo));
-            int index1 = i + 1;
+        for (Point p : points) {
+            Arrays.sort(pointsCopy, p.slopeOrder().thenComparing(Point::compareTo));
+            int index1 = 0;
             int index2 = index1 + 2;
             while (index2 < len) {
                 Point p1 = pointsCopy[index1];
@@ -35,11 +34,11 @@ public class FastCollinearPoints {
                 if (Double.compare(s1, s2) == 0) {
                     index2++;
                     if (index2 == len && index2 - index1 > 2) {
-                        addSeg(min(p, p1), max(p, pointsCopy[index2 - 1]));
+                        addSegIfAbsent(p, p1, pointsCopy[index2 - 1]);
                     }
                 } else {
                     if (index2 - index1 > 2) {
-                        addSeg(min(p, p1), max(p, pointsCopy[index2 - 1]));
+                        addSegIfAbsent(p, p1, pointsCopy[index2 - 1]);
                         index1 = index2;
                         index2 = index1 + 2;
                     } else {
@@ -58,26 +57,23 @@ public class FastCollinearPoints {
 
         // make sure not null and not equal
         for (int i = 0; i < points.length; i++) {
+            Point p1 = points[i];
+            if (p1 == null) {
+                throw new IllegalArgumentException();
+            }
             for (int j = i + 1; j < points.length; j++) {
-                Point p1 = points[i];
                 Point p2 = points[j];
-                if (p1 == null || p2 == null || p1.compareTo(p2) == 0) {
+                if (p2 == null || p1.compareTo(p2) == 0) {
                     throw new IllegalArgumentException();
                 }
             }
         }
     }
 
-    private void addSeg(Point start, Point end) {
-        segments.enqueue(new LineSegment(start, end));
-    }
-
-    private Point min(Point p1, Point p2) {
-        return p1.compareTo(p2) > 0 ? p2 : p1;
-    }
-
-    private Point max(Point p1, Point p2) {
-        return p1.compareTo(p2) > 0 ? p1 : p2;
+    private void addSegIfAbsent(Point current, Point start, Point end) {
+        if (current.compareTo(start) < 0) {
+            segments.enqueue(new LineSegment(current, end));
+        }
     }
 
     // the number of line segments
@@ -111,7 +107,7 @@ public class FastCollinearPoints {
         StdDraw.enableDoubleBuffering();
         StdDraw.setXscale(0, 32768);
         StdDraw.setYscale(0, 32768);
-        StdDraw.setPenRadius(0.01);
+        StdDraw.setPenRadius(0.001);
         for (Point p : points) {
             p.draw();
         }
