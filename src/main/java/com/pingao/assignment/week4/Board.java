cb565474d17b4921d12b1d4dd28ee3ecdd76ebde
wocanmei <pingao777@gmail.com>
2018-07-01 19:38:14 +0800

    第四周作业初始化

---
+++ src/main/java/com/pingao/assignment/week4/Board.java
@@ -0,0 +1,57 @@
+package com.pingao.assignment.week4;
+
+/**
+ * Created by pingao on 2018/7/1.
+ */
+public class Board {
+    // construct a board from an n-by-n array of blocks
+    public Board(int[][] blocks) {
+
+    }
+
+    // (where blocks[i][j] = block in row i, column j)
+    // board dimension n
+    public int dimension() {
+
+    }
+
+    // number of blocks out of place
+    public int hamming() {
+
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+
+    }
+
+    // a board that is obtained by exchanging any pair of blocks
+    public Board twin() {
+
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+
+    }
+
+    // string representation of this board (in the output format specified below)
+    public String toString() {
+
+    }
+
+    // unit tests (not graded)
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/pingao/assignment/week4/Solver.java
@@ -0,0 +1,31 @@
+package com.pingao.assignment.week4;
+
+/**
+ * Created by pingao on 2018/7/1.
+ */
+public class Solver {
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial) {
+
+    }
+
+    // is the initial board solvable?
+    public boolean isSolvable() {
+
+    }
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
+
+    }
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution() {
+
+    }
+
+    // solve a slider puzzle (given below)
+    public static void main(String[] args) {
+
+    }
+}
