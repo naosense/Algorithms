d443af8941eadba84b40f9f2f808826c3ab1b51c
wocanmei <pingao777@gmail.com>
2018-12-10 16:04:46 +0800

    week10作业初始化

---
+++ src/main/java/com/pingao/assignment/week10/CircularSuffixArray.java
@@ -0,0 +1,26 @@
+package com.pingao.assignment.week10;
+
+/**
+ * Created by pingao on 2018/12/10.
+ */
+public class CircularSuffixArray {
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+
+    }
+
+    // length of s
+    public int length() {
+
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/pingao/assignment/week10/MoveToFront.java
@@ -0,0 +1,22 @@
+package com.pingao.assignment.week10;
+
+/**
+ * Created by pingao on 2018/12/10.
+ */
+public class MoveToFront {
+    // apply move-to-front encoding, reading from standard input and writing to standard output
+    public static void encode() {
+
+    }
+
+    // apply move-to-front decoding, reading from standard input and writing to standard output
+    public static void decode() {
+
+    }
+
+    // if args[0] is '-', apply move-to-front encoding
+    // if args[0] is '+', apply move-to-front decoding
+    public static void main(String[] args) {
+
+    }
+}
